b6bfdbbe54f7a1a74d8a7992d2466b34a9b621a7
faustofusse <faustofusse@gmail.com>
2021-04-21 22:31:34 -0300

    Email verification:

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.render('index', { title: 'myPro' });
 });
+
+router.get('/verify/:token', (req, res) => {
+  const { token } = req.params;
+  const name = jwt.decode(token).firstName;
+  res.render('verify', { name: name, title: 'Verify user' })
+})
 
 module.exports = router;
